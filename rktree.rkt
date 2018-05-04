cf8d2635014b507d01f055736cca139754cc9886
Charlie Wang <s455wang@csclub.uwaterloo.ca>
2018-05-04 09:01:29 -0400

    rktree WIP

--- rktree.rkt
+++ rktree.rkt
@@ -6,6 +6,9 @@
                   curry)
          (only-in racket/path
                   path-has-extension?)
+         (only-in racket/function
+                  identity
+                  thunk)
          racket/cmdline)
 
 
@@ -16,7 +19,8 @@
 ;; produce the line of text in markdown formatting
 ;; of path, as it would appear in a directory tree list
 ;; with links, eg.  "    - [foo.txt](/home/charlie/foo.txt)"
-(define (md-list-line path)
+;; TODO: use directory? predicate
+(define (md-list-line path [directory? (directory-exists? path)])
   (define exploded-path (explode-path/with-empty path))
   (cond
     [(null? exploded-path) ""]
@@ -33,12 +37,35 @@
 (define (path-has-extension?* path exts)
   (ormap (curry path-has-extension? path) exts))
 
+;; generate the markdown for a directory, recursively
+(define (generate-md dir whitelist output)
+  (parameterize ([current-directory dir])
+    (with-output-to-file output
+      (thunk
+       ;; produce a list of directories and false,
+       ;; for the recursive call
+       (filter
+        identity
+        (for/list ([path (in-directory)])
+          (define directory? (directory-exists? path))
+          (when (or (null? (whitelist))
+                    directory?
+                    (path-has-extension?* path (whitelist)))
+            (displayln (md-list-line path)))
+          (and directory? path))))
+      ;; TODO: recursive call on dirs here
+      )))
+
 (module+ main
   (define title (make-parameter #f))
   (define whitelist (make-parameter null))
+  (define output-name (make-parameter "dir.md"))
   (define dir
     (command-line
      #:once-each
+     [("-o" "--output") output-filename
+                        "File name for output"
+                        (output-name output-filename)]
      [("-T" "--title") title-text "Title" (title title-text)]
      #:multi
      [("-w" "--whitelist") file-ext "File extension (with dot)"
@@ -50,9 +77,4 @@
      dir))
   (when (title)
     (printf "# ~a\n\n" (title)))
-  (parameterize ([current-directory dir])
-    (for ([path (in-directory)])
-      (when (or (null? (whitelist))
-                (directory-exists? path)
-                (path-has-extension?* path (whitelist)))
-        (displayln (md-list-line path))))))
+  (generate-md dir whitelist output-name))
